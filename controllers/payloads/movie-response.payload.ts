66bede1f55c7209d4fe1f886c83336b1393a3509
Jitender Chaudhary <108271961+jitender-chaudhary@users.noreply.github.com>
2022-07-14 14:35:19 +0530

    Revert "Feature/sloths 6 api to get movies by cityid"

--- controllers/payloads/movie-response.payload.ts
+++
@@ -1,5 +0,0 @@
-export default class MovieResponsePayload {
-  id: String = '';
-
-  name: String = '';
-}
--- controllers/payloads/theatre-response.payload.ts
+++
@@ -1,4 +0,0 @@
-export default class TheatreResponsePayload {
-  id: String = '';
-  name: String = '';
-}
