1479f03af6296e600373667b940f8fb073010696
Tushar Swarup Tandon <tushar.tandon@cawstudios.com>
2022-07-18 12:19:31 +0530

    adding api functionality

---
+++ src/repositories/entities/migrations/20220715081923_initial_structure/migration.sql
@@ -0,0 +1,136 @@
+-- CreateTable
+CREATE TABLE "theatre" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(64) NOT NULL,
+    "cityId" TEXT NOT NULL,
+    "numberOfScreens" INTEGER NOT NULL,
+
+    CONSTRAINT "theatre_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "city" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(64) NOT NULL,
+
+    CONSTRAINT "city_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "screen" (
+    "id" TEXT NOT NULL,
+    "theatreId" TEXT NOT NULL,
+    "screenNumber" INTEGER NOT NULL,
+
+    CONSTRAINT "screen_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "show" (
+    "id" TEXT NOT NULL,
+    "screenId" TEXT NOT NULL,
+    "movieId" TEXT NOT NULL,
+    "showStartTimeInUtc" TIMESTAMP(3) NOT NULL,
+    "showEndTimeInUtc" TIMESTAMP(3) NOT NULL,
+    "availableUntilUtc" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "show_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "movie" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(128) NOT NULL,
+    "durationInMins" INTEGER NOT NULL,
+    "language" VARCHAR(32) NOT NULL,
+
+    CONSTRAINT "movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "seat" (
+    "id" TEXT NOT NULL,
+    "screenId" TEXT NOT NULL,
+    "seatNumber" INTEGER NOT NULL,
+
+    CONSTRAINT "seat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bookedSeat" (
+    "id" TEXT NOT NULL,
+    "seatId" TEXT NOT NULL,
+    "showId" TEXT NOT NULL,
+    "bookingId" TEXT NOT NULL,
+
+    CONSTRAINT "bookedSeat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "booking" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "showId" TEXT NOT NULL,
+    "numberOfSeatsBooked" INTEGER NOT NULL,
+    "bookedOnDate" DATE NOT NULL,
+
+    CONSTRAINT "booking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(128) NOT NULL,
+    "email" VARCHAR(64) NOT NULL,
+    "cityId" TEXT,
+    "phoneNumber" VARCHAR(16) NOT NULL,
+    "loggedInAtUtc" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "username" VARCHAR(128) NOT NULL,
+    "passwordHash" VARCHAR(128) NOT NULL,
+
+    CONSTRAINT "account_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "theatre" ADD CONSTRAINT "theatre_cityId_fkey" FOREIGN KEY ("cityId") REFERENCES "city"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "screen" ADD CONSTRAINT "screen_theatreId_fkey" FOREIGN KEY ("theatreId") REFERENCES "theatre"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "show" ADD CONSTRAINT "show_screenId_fkey" FOREIGN KEY ("screenId") REFERENCES "screen"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "show" ADD CONSTRAINT "show_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "movie"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "seat" ADD CONSTRAINT "seat_screenId_fkey" FOREIGN KEY ("screenId") REFERENCES "screen"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookedSeat" ADD CONSTRAINT "bookedSeat_showId_fkey" FOREIGN KEY ("showId") REFERENCES "show"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookedSeat" ADD CONSTRAINT "bookedSeat_seatId_fkey" FOREIGN KEY ("seatId") REFERENCES "seat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookedSeat" ADD CONSTRAINT "bookedSeat_bookingId_fkey" FOREIGN KEY ("bookingId") REFERENCES "booking"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "booking" ADD CONSTRAINT "booking_showId_fkey" FOREIGN KEY ("showId") REFERENCES "show"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "booking" ADD CONSTRAINT "booking_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_cityId_fkey" FOREIGN KEY ("cityId") REFERENCES "city"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "account" ADD CONSTRAINT "account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
