59d3b84e1cf1c4dfed980b1c296ef9344d0b6891
Tushar Swarup Tandon <tushar.tandon@cawstudios.com>
2022-07-18 17:18:42 +0530

    added extra variable in hash.helper

--- src/common/helpers/hash.helper.ts
+++ src/common/helpers/hash.helper.ts
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 export default class HashHelper {
   public async hashString(stringToHash : String) : Promise<String> {
     const salt = await bcrypt.genSalt(12);
-    return bcrypt.hash(stringToHash, salt);
+    const hashedString = await bcrypt.hash(stringToHash, salt);
+    return hashedString;
   }
 }
