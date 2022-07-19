9c3b47ef35e6a6b7ed9f53134e1ffcc806ffe8ab
Rishi Datta <rishi.datta@cawstudios.com>
2022-07-19 15:31:37 +0530

    made changes

--- src/common/helpers/jwtToken.helper.ts
+++ src/common/helpers/jwtToken.helper.ts
@@ -3,13 +3,12 @@ import AccessTokenModel from '../models/accessToken.model';
 
 export default class JwtTokenHelper {
   public async generateToken(accessToken: AccessTokenModel): Promise<string> {
-    const token :string = jwt.sign(
+    return jwt.sign(
       { accessToken },
-      '',
+      String(process.env.ACCESS_TOKEN_SECRET),
       {
         expiresIn: process.env.JWT_EXPIRES_IN,
       },
     );
-    return token;
   }
 }
