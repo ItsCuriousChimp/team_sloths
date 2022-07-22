fb2e67d673cb641e03267e2f642243aef91c74db
Rishi Datta <rishi.datta@cawstudios.com>
2022-07-22 13:01:57 +0530

    added async storage helper

--- src/common/helpers/requestContext.helper.ts
+++ src/common/helpers/requestContext.helper.ts
@@ -1,15 +1,15 @@
 import { AsyncLocalStorage } from 'async_hooks';
 
-export default class RequestContextHelper {
+export default class AsyncStorage {
   private static storage: Map<String, any> = new Map();
   public getItems() {
     const asyncLocalStorage = new AsyncLocalStorage();
-    return asyncLocalStorage.run(RequestContextHelper.storage, () => asyncLocalStorage.getStore());
+    return asyncLocalStorage.run(AsyncStorage.storage, () => asyncLocalStorage.getStore());
   }
 
   public setItem(key: string, value: any) {
     const asyncLocalStorage = new AsyncLocalStorage();
-    asyncLocalStorage.run(RequestContextHelper.storage, () => {
+    asyncLocalStorage.run(AsyncStorage.storage, () => {
       const store: any = asyncLocalStorage.getStore();
       store.set(key, value);
     });
