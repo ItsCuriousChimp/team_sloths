f3ff7e76594af5d31941d518ca5397413e317ec4
jitender-chaudhary <jitender.chaudhary@cawstudios.com>
2022-07-25 02:01:57 +0530

    removed comments

--- src/common/models/user.model.ts
+++ src/common/models/user.model.ts
@@ -11,13 +11,11 @@ export default class UserModel {
     id : String,
     name: String,
     email: String,
-    // city:String,
     loggedInAtUtc : Date,
   ) {
     this.id = id;
     this.name = name;
     this.email = email;
-    // this.city = city;
     this.loggedInAtUtc = loggedInAtUtc;
   }
 }
