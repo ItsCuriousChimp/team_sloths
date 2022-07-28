9ed9ce45ac19026d2139ba142a6fd9b2eade4d3e
Tushar Swarup Tandon <tushar.tandon@cawstudios.com>
2022-07-28 12:26:54 +0530

    changing String to string in hash helper

--- src/common/helpers/hash.helper.ts
+++ src/common/helpers/hash.helper.ts
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 
 export default class HashHelper {
-  public async hashString(stringToHash : String) : Promise<string> {
+  public async hashString(stringToHash : string) : Promise<string> {
     const salt = await bcrypt.genSalt(12);
     const hashedString = await bcrypt.hash(stringToHash, salt);
     return hashedString;
