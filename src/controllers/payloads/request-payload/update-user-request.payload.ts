74e12c7384ac5c72a23b779afa234d5bcbd8b520
Tushar Swarup Tandon <tushar.tandon@cawstudios.com>
2022-07-29 12:59:59 +0530

    updating joi schema for phone number

--- src/controllers/payloads/request-payload/update-user-request.payload.ts
+++ src/controllers/payloads/request-payload/update-user-request.payload.ts
@@ -5,7 +5,7 @@ export default class UpdateUserRequestPayload {
   @JoiSchema(Joi.string().min(3).optional())
     name: string = '';
 
-  @JoiSchema(Joi.string().length(10).optional())
+  @JoiSchema(Joi.string().length(10).pattern(/^[0-9]+$/).optional())
     phoneNumber: string = '';
 
   @JoiSchema(Joi.string().guid().optional())
