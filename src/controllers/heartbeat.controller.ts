e10af63da5c3f1a360741aaa1bff4494e9cbae75
Rishi Datta <rishi.datta@cawstudios.com>
2022-08-01 12:04:37 +0530

    added docker

--- src/controllers/heartbeat.controller.ts
+++ src/controllers/heartbeat.controller.ts
@@ -1,13 +1,14 @@
+import { Request, Response } from 'express';
 import HeartbeatResponsePayload from './payloads/heartbeat-response.payload';
 import HeartbeatServices from '../services/heartbeat.service';
 
 export default class HeartbeatController {
-  public getHeartbeat(): HeartbeatResponsePayload {
+  public getHeartbeat(req: Request, res: Response) {
     const heartbeatService = new HeartbeatServices();
     const heartbeatResult = heartbeatService.getBeat();
     const beat = heartbeatResult.lastHeartbeatAtTimestamp;
     const payload = new HeartbeatResponsePayload();
     payload.heartbeatTimestamp = beat;
-    return payload;
+    res.json(payload);
   }
 }
