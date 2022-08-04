63c6ec3e8757eb179bfccdbc467a6866c762c14b
Rishi Datta <rishi.datta@cawstudios.com>
2022-08-04 14:48:34 +0530

    added transactions

--- src/repositories/transaction-middleware.repository.ts
+++ src/repositories/transaction-middleware.repository.ts
@@ -6,8 +6,8 @@ export default class TransactionsMiddleware {
   async transactionMiddleware(body: any) {
     const response = await prisma.$transaction(
       async (prismaInstance: any): Promise<any> => {
-        const resp = await body(prismaInstance);
-        return resp;
+        const res = await body(prismaInstance);
+        return res;
       },
     );
     return response;
