a393449901682062bff5918ab1c9fc9fa955c419
Tushar Swarup Tandon <tushar.tandon@cawstudios.com>
2022-08-08 13:43:00 +0530

    adding global error handler

--- src/common/errors/argument-validation.error.ts
+++ src/common/errors/argument-validation.error.ts
@@ -1,9 +1,7 @@
 import BaseError from './base.error';
 
 export default class ArgumentValidationError extends BaseError {
-  message : string;
   constructor(message : string) {
     super('ArgumentValidationError', message);
-    this.message = message;
   }
 }
--- src/common/errors/unauthorized.error.ts
+++ src/common/errors/unauthorized.error.ts
@@ -1,10 +1,7 @@
 import BaseError from './base.error';
 
 export default class UnauthorizedError extends BaseError {
-  name : string = 'UnauthorizedError';
-  message : string;
   constructor(message : string) {
     super('UnauthorizedError', message);
-    this.message = message;
   }
 }
