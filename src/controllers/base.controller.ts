1859287e4aba3f86fa67d9f7ded07ffe003f3e2d
Tushar Swarup Tandon <108271127+itsTusharHere@users.noreply.github.com>
2022-08-11 14:51:01 +0530

    Update base.controller.ts

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -1,6 +1,5 @@
 import { getClassSchema } from 'joi-class-decorators';
 import ArgumentValidationError from '../common/errors/argument-validation.error';
-// import { E0101 } from '../common/errors/error-codes';
 
 export default class BaseController {
   public extractAndValidate(src: any, destClass: any) {
